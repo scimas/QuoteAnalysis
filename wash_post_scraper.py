f1ffaf6e7e0ac1899fcaa6204f77c24030ad2ab8
Mihir Gadgil <16473290+scimas@users.noreply.github.com>
2019-11-02 12:06:38 -0400

    Work with lists of urls

--- wash_post_scraper.py
+++ wash_post_scraper.py
@@ -4,12 +4,15 @@ from bs4 import BeautifulSoup
 
 
 def get_article(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "lxml")
-    article = soup.find("div", attrs={"class": "article-body"})
+    articles = []
+    for ur in url:
+        page = requests.get(ur)
+        soup = BeautifulSoup(page.content, "lxml")
+        article = soup.find("div", attrs={"class": "article-body"})
+        
+        text = ""
+        for para in article.find_all(["p", "h3"]):
+            text += para.text + "\n"
+        articles.append(text)
     
-    text = ""
-    for para in article.find_all(["p", "h3"]):
-        text += para.text + "\n"
-    
-    return text
+    return articles
