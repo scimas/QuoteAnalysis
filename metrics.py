133aabb755abde32d973751d397aba1874a4f493
Mary Gibbs <mgibbs1259@gmail.com>
2019-11-09 14:00:46 -0500

    pulled

--- metrics.py
+++ metrics.py
@@ -1,10 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from numpy.linalg import norm
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import TreebankWordTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans
+
 
 tokenizer = TreebankWordTokenizer()
 eng_stopwords = stopwords.words("english")
@@ -32,3 +35,17 @@ def CosineSimilarity(sent1, sent2):
     sents = vectorizer.fit_transform([sent1, sent2]).toarray()
 
     return sents[0].dot(sents[1].T)/norm(sents[0])/norm(sents[1])
+
+
+def KMeans(quote_corpus, clusters=2):
+    # Add stemming, tokenizer
+    vectorizer = CountVectorizer(lowercase=True, stop_words=stopwords.words('english'), max_df=0.5, binary=True)
+    X = vectorizer.fit_transform(quote_corpus)
+    kmeans_model = KMeans(n_clusters=clusters)
+
+    print("Top terms per cluster:")
+    order_centroids = kmeans_model.cluster_centers_.argsort()[:, ::-1]
+    terms = X.get_feature_names()
+    for i in range(clusters):
+        top_ten_words = [terms[ind] for ind in order_centroids[i, :5]]
+        print("Cluster {}: {}".format(i, ' '.join(top_ten_words)))
