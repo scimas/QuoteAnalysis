0912545571bb0a625df7b3f91f715fa697ef55b1
Mihir Gadgil <16473290+scimas@users.noreply.github.com>
2019-11-10 12:35:15 -0500

    Improvements to metrics

--- metrics.py
+++ metrics.py
@@ -7,28 +7,42 @@ from nltk.tokenize import TreebankWordTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 
 tokenizer = TreebankWordTokenizer()
-eng_stopwords = stopwords.words("english")
+eng_stopwords = tuple(stopwords.words("english"))
 stemmer = PorterStemmer()
 
-def preprocessing(sent):
+
+def tokenization(sent):
+    """
+    @author: Mihir Gadgil
+    Tokenizer for vectorizing sentences.
+    """
+    # Tokenize sentence
     tokens = tokenizer.tokenize(sent)
-    s = [token for token in tokens if not(token in eng_stopwords)]
-    processed_sent = ""
-    for token in s:
-        processed_sent += token + " "
-    processed_sent = processed_sent.strip()
+    # Remove stopwords and stemming
+    processed_tokens = [stemmer.stem(token) for token in tokens if token not in eng_stopwords]
+    return processed_tokens
 
-    return stemmer.stem(processed_sent)
 
-vectorizer = CountVectorizer(tokenizer=tokenizer.tokenize, binary=True, preprocessor=preprocessing)
+# Text vectorizer for CosineSimilarity
+vectorizer = CountVectorizer(tokenizer=tokenization, binary=True)
+
 
 def JaccardSimilarity(sent1, sent2):
+    """
+    @author: Mihir Gadgil
+    Sentence Jaccard similarity.
+    """
     tokens1 = set(tokenizer.tokenize(sent1))
     tokens2 = set(tokenizer.tokenize(sent2))
 
     return len(tokens1.intersection(tokens2)) / len(tokens1.union(tokens2))
 
+
 def CosineSimilarity(sent1, sent2):
+    """
+    @author: Mihir Gadgil
+    Sentence cosine similarity.
+    """
     sents = vectorizer.fit_transform([sent1, sent2]).toarray()
 
-    return sents[0].dot(sents[1].T)/norm(sents[0])/norm(sents[1])
+    return np.matmul(sents[0], sents[1].T)/norm(sents[0])/norm(sents[1])
