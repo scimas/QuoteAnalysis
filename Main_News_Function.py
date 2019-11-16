4f90bbecbb70875563f27dbca6f0913f78e2a953
BINBINWU <bxw10a@acu.edu>
2019-11-16 13:09:56 -0500

    Final quote analysis

--- Main_News_Function.py
+++ Main_News_Function.py
@@ -16,6 +16,7 @@ import spacy
 import scrapers
 import metrics
 import quote_extraction
+from nltk.corpus import stopwords
 
 
 #%% Functions
@@ -44,9 +45,14 @@ def get_Stories(api_response_json):
 ###############################################################################
 
 def Google_quote(quote) :
+    remove_vids = 'site:www.cnn.com -site:cnn.com/video -site:cnn.com/videos -site:cnn.com/shows -site:foxnews.com/shows -site:breitbart.com/tag'
     matching_quote_url=[]
-    for url in search(quote, stop=20):
-        matching_quote_url.append(url)
+    domains=['www.foxnews.com','www.cnn.com','www.bbc.com','www.breitbart.com','www.apnews.com','www.washingtonpost.com']
+    for domain in domains:
+        for url in search(quote+remove_vids, domains=domain, tbs="qdr:m" , stop=3):
+            matching_quote_url.append(url)
+            time.sleep(3)
+        time.sleep(120)
     return matching_quote_url
 
 
@@ -88,76 +94,72 @@ def text_from_Google_url(Google_urls):
             'ap':ap_article
             }
 
-
 tokenizer = TreebankWordTokenizer()
-# Didn't work on the below:
+
+
+###############################################################################
+# MAIN Functions
+###############################################################################
+
+def main(og_source, topic, start_time,end_time ):
+    #test=news_api('fox-news','trump AND impeach','2019-10-31','2019-11-02')
+    news= news_api(og_source, topic, start_time, end_time)
+    urls=get_Stories(news)
+    article_list=[]
+    for i in urls:
+        article_list.append(scrapers.get_article_fox(i))
+
+    quotes=[]
+    for article in article_list:
+        quotes.append(quote_extraction.find_quotes_in_text(article))
+
+    quotes_list = [y for x in quotes for y in x]
+
+    # filiter quotes_list 3 words withou stop_words
+    stop_words= tuple(stopwords.words('english'))
+    quotes_list_filiter=[]
+    for quotes in quotes_list:
+        quote_len = 0
+        for token in tokenizer.tokenize(quotes):
+            if token not in stop_words:
+                quote_len+=1
+        if quote_len > 3:
+            quotes_list_filiter.append(quotes)
+    urls=[]
+    for google_quote in quotes_list_filiter:
+        urls.append(Google_quote(google_quote))
+
+    dictionary = text_from_Google_url(urls)
+
+    dictionary_quotes={}
+    for i in dictionary:
+        quotes=[]
+        if (len(dictionary[i])!=0):
+            for article in dictionary[i]:
+                quotes.append(find_quotes_in_text(article))
+        dictionary_quotes[i] = quotes
+
+    similarity_result={}
+    sources =['fox','bb','cnn','bbc','wp','ap']
+    for quote in quotes_list:
+        for source in sources:
+            for dictionary_quotes_clust in dictionary_quotes[source]:
+                # filiter dictionary_quotes. 60% length match without stop_words
+
+                for google_quote in dictionary_quotes_clust:
+                    google_quote_len=0
+                    for google_token in tokenizer.tokenize(google_quote):
+                        if google_token not in stop_words:
+                            google_quote_len += 1
+
+                    if google_quote_len > 3:
+                        if (og_source, source) not in similarity_result.keys():
+                            similarity_result[(og_source, source)] = [JaccardSimilarity(quote, google_quote)]
+                        else:
+                            similarity_result[(og_source, source)].append(JaccardSimilarity(quote, google_quote))
+    return similarity_result
 
 
 
+print(main('fox-news','trump AND impeach','2019-10-31','2019-11-02'))
 
-
-
-test=news_api('fox-news','trump AND impeach','2019-10-31','2019-11-02')
-
-
-
-
-urls=get_Stories(test)
-
-
-
-
-article_list=[]
-for i in urls:
-    article_list.append(scrapers.get_article_fox(i))
-
-
-
-article_list[1]
-
-
-
-quotes=[]
-for article in article_list:
-    quotes.append(quote_extraction.find_quotes_in_text(article))
-
-
-
-quotes1 = [y for x in quotes for y in x]
-
-
-
-
-len(quotes1)
-
-
-
-
-for i in search(quotes1[0],stop=10):
-    print (i)
-
-
-
-
-
-dictionary = text_from_Google_url(google_urls)
-
-
-
-quote="The Greatest Witch Hunt in American History!"
-def Google_quote(quote) :
-    matching_quote_url=[]
-    for url in search(quote, stop=20):
-        matching_quote_url.append(url)
-    return matching_quote_url
-
-Google_quote(quote)
-
-
-
-#entity
-import spacy
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("The Greatest Witch Hunt in American History!")
-for ent in doc.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
